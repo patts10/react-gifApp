d299a846fa6fc5355333d3739de5ce9d450f55f4
Christian Mendoza <patts2810@gmail.com>
2022-02-03 12:56:37 -0500

    feat: create gift app

---
+++ src/GifExpertApp.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['Squartle']);
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={ setCategories } />
+      <hr />
+
+      {
+        categories.map( category => (
+          <GifGrid 
+            key={ category } 
+            category={ category }
+          />
+        ))
+      }
+    </>
+  );
+};
---
+++ src/components/AddCategory.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const AddCategory = ({ setCategories }) => {
+
+  const [inputvalue, setInputvalue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputvalue(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputvalue.trim().length > 2) {
+      setCategories( c => [ inputvalue, ...c ]);
+      setInputvalue('');
+    }
+  }
+
+  return (
+  <form onSubmit={ handleSubmit } >
+    <input 
+      type='text'
+      value={ inputvalue }
+      onChange={ handleInputChange }
+    />
+  </form>
+  );
+};
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired
+}
